cf5430869d4a35f000913cfce5cbd746e5aa64ee
KiraYous <404465394@qq.com>
2022-08-03 14:02:18 +0800

    feat : use route to locate the url page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import TodoList from './features/todoList/TodoList';
+import Layout from './layout/Layout';
+import {Route,Routes} from "react-router-dom"
+import HelpPage from './pages/HelpPage';
+import DonePage from './pages/DonePage';
+import NoFoundPage from './pages/NoFoundPage';
 
 function App() {
   return (
     <div className="App">
-        <h1>
+        {/* <h1>
           TodoList
-        </h1>
-        <TodoList/>
+        </h1> */}
+        <Routes>
+          <Route path='/' element={<Layout/>}>
+            <Route  index element={<TodoList/>}></Route>
+            <Route path='help' element={<HelpPage/>}></Route>
+            <Route path='done' element={<DonePage/>}></Route>
+            <Route path='*' element={<NoFoundPage/>}></Route>
+          </Route>
+        </Routes>
     </div>
   );
 }
